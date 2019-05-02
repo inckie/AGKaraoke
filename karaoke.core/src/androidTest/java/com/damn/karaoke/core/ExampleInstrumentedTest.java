bf3362852d14d5a4e930418492ae2b087b355fa0
Ink <i@am>
2019-05-02 18:54:31 +0300

    + benchmark

--- karaoke.core/src/androidTest/java/com/damn/karaoke/core/ExampleInstrumentedTest.java
+++ karaoke.core/src/androidTest/java/com/damn/karaoke/core/ExampleInstrumentedTest.java
@@ -1,13 +1,17 @@
 package com.damn.karaoke.core;
 
-import android.content.Context;
-import android.support.test.InstrumentationRegistry;
 import android.support.test.runner.AndroidJUnit4;
+import android.util.Log;
+
+import com.damn.karaoke.core.controller.processing.AutoCorrelationToneDetector;
+import com.damn.karaoke.core.controller.processing.GoertzelToneDetector;
+import com.damn.karaoke.core.controller.processing.GoertzelToneDetectorJNI;
+import com.damn.karaoke.core.controller.processing.IToneDetector;
 
 import org.junit.Test;
 import org.junit.runner.RunWith;
 
-import static org.junit.Assert.*;
+import java.nio.ShortBuffer;
 
 /**
  * Instrumented test, which will execute on an Android device.
@@ -16,11 +20,27 @@ import static org.junit.Assert.*;
  */
 @RunWith(AndroidJUnit4.class)
 public class ExampleInstrumentedTest {
-    @Test
-    public void useAppContext() throws Exception {
-        // Context of the app under test.
-        Context appContext = InstrumentationRegistry.getTargetContext();
 
-        assertEquals("com.damn.karaoke.core.test", appContext.getPackageName());
+    @Test
+    public void benchMark() throws Exception {
+        int data_len = 16365;
+        ShortBuffer buffer = ShortBuffer.allocate(data_len);
+        for(int i = 0; data_len != i; ++i)
+            buffer.put((short) (12000 * Math.sin(440 * i * 2 * Math.PI / data_len)));
+
+        short[] data = buffer.array();
+
+        bench(new GoertzelToneDetector(44100, 1, 1), data);
+        bench(new GoertzelToneDetectorJNI(44100, 1, 1), data);
+        bench(new AutoCorrelationToneDetector(44100, 1, 1), data);
+    }
+
+    private void bench(IToneDetector detector, short[] data) {
+        Log.d("TEST", detector.getClass().getName() + " started");
+        long l = System.currentTimeMillis();
+        for(int i = 0; 16 != i; ++i)
+            detector.analyze(data, data.length);
+        long delta = System.currentTimeMillis() - l;
+        Log.d("TEST", detector.getClass().getName() + ": " + delta);
     }
 }
